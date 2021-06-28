fe05507160a67f38646c77bdf225d49590fadaea
adriangrpz <adrian.garcia04@ciencias.unam.mx>
2021-06-27 23:52:53 -0500

    Color recursive

--- Tarea5/src/Interface.py
+++ Tarea5/src/Interface.py
@@ -43,7 +43,8 @@ class Interface(Frame):
         fileMenu.add_command(label='Save',          command=self.saveImg)
         toolBar.add_cascade(label='File',           menu=fileMenu)
 
-        toolBar.add_command(label='Procesar',      command=self.proccess)
+        toolBar.add_command(label='Procesar blanco y negro',      command=self.proccessBW)
+        toolBar.add_command(label='Procesar color',      command=self.proccessColor)
         toolBar.add_command(label='Exit',      command=self.exit)
 
         self.root.config(menu=toolBar)
@@ -61,7 +62,7 @@ class Interface(Frame):
     """
     Function that asks the user for a custom text and the size of the squares.
     """
-    def proccess(self):
+    def proccessBW(self):
         self.top = Toplevel()
 
         Label(self.top, text="Tamaño cuadrícula").pack()
@@ -91,12 +92,47 @@ class Interface(Frame):
         self.buttontext = StringVar()
         self.buttontext.set("Apply")
 
-        self.button = Button(self.top, textvariable=self.buttontext, command=lambda: self.proccessConfirm(x1Val, y1Val, x2Val, y2Val)).pack()
+        self.button = Button(self.top, textvariable=self.buttontext, command=lambda: self.proccessBWConfirm(x1Val, y1Val, x2Val, y2Val)).pack()
+
+    """
+    Function that asks the user for a custom text and the size of the squares.
+    """
+    def proccessColor(self):
+        self.top = Toplevel()
+
+        Label(self.top, text="Tamaño cuadrícula").pack()
+
+        Label(self.top, text="x").pack()
+        x1Val = IntVar()
+        x1Val.set(self.gridSize[0])
+        Entry(self.top, textvariable=x1Val).pack()
+
+        Label(self.top, text="y").pack()
+        y1Val = IntVar()
+        y1Val.set(self.gridSize[1])
+        Entry(self.top, textvariable=y1Val).pack()
+
+        Label(self.top, text="Tamaño de cada cuadrícula de salida").pack()
+
+        Label(self.top, text="x").pack()
+        x2Val = IntVar()
+        x2Val.set(self.outPixelsSize[0])
+        Entry(self.top, textvariable=x2Val).pack()
+
+        Label(self.top, text="y").pack()
+        y2Val = IntVar()
+        y2Val.set(self.outPixelsSize[1])
+        Entry(self.top, textvariable=y2Val).pack()
+
+        self.buttontext = StringVar()
+        self.buttontext.set("Apply")
+
+        self.button = Button(self.top, textvariable=self.buttontext, command=lambda: self.proccessColorConfirm(x1Val, y1Val, x2Val, y2Val)).pack()
 
     """
     Function that checks the values given by the user and then applies the text filter.
     """
-    def proccessConfirm(self, x1Val, y1Val, x2Val, y2Val):
+    def proccessBWConfirm(self, x1Val, y1Val, x2Val, y2Val):
         x1 = None
         x2 = None
         y1 = None
@@ -213,6 +249,7 @@ class Interface(Frame):
         newImg = PImage.new('RGB', (newSizeX, newSizeY), (255, 255, 255, 255))
 
         imgspool = [PImage.open(f'out/{x}.jpg') for x in range(30)]
+        imgspool = [img.resize(size) for img in imgspool]
 
         for index in whatImages:
             if index == -1:
@@ -220,8 +257,6 @@ class Interface(Frame):
                 posY += size[1]
             else:
                 img = imgspool[index]
-                img = img.resize(size)
-
                 newImg.paste(img, (posX, posY))                
                 posX += size[0]
         
@@ -229,6 +264,99 @@ class Interface(Frame):
         
         return toModify
 
+    def proccessColorConfirm(self, x1Val, y1Val, x2Val, y2Val):
+        x1 = None
+        x2 = None
+        y1 = None
+        y2 = None
+        try:
+            x1 = x1Val.get()
+            x2 = x2Val.get()
+
+            y1 = y1Val.get()
+            y2 = y2Val.get()
+
+            x1 = int(x1)
+            x2 = int(x2)
+            
+            y1 = int(y1)
+            y2 = int(y2)
+            if x1 <= 0 or y1 <= 0 or x2 <= 0 or y2 <= 0:
+                raise Exception()
+        except Exception as e:
+            self.message('Error', 'The values should be integers.')
+            return
+        self.gridSize = x1, y1
+        self.outPixelsSize = x2, y2
+        
+        newImg = self.proccessColorFilter()
+        self.update(newImg)
+        self.top.destroy()
+
+    def proccessColorFilter(self):
+        newImg = self.image.original
+        toModify = self.image.modified
+
+        rgbColors = toModify.convert('RGB')
+        pixels = newImg.load()
+
+        corners = getMosaicCorners(toModify.size, self.gridSize[0], self.gridSize[1])
+
+        newSizeX = (int) (newImg.size[0] / self.gridSize[0]) * self.outPixelsSize[0]
+        newSizeY = (int) (newImg.size[1] / self.gridSize[1]) * self.outPixelsSize[1]
+
+        total = len(corners)
+
+        outputImage = PImage.new('RGB', (newSizeX, newSizeY), (255, 255, 255, 255))
+
+        posX = 0
+        posY = 0
+
+        for (i, square) in enumerate(corners):
+            xLeft, yLeft, xRight, yRight = square
+            rAvg = 0
+            gAvg = 0
+            bAvg = 0
+            count = 1
+            for col in range(xLeft, xRight):
+                for row in range(yLeft, yRight):
+                    r, g, b = rgbColors.getpixel((col, row))
+                    
+                    rAvg += r
+                    gAvg += g
+                    bAvg += b
+                    count += 1
+
+            rAvg = rAvg // count
+            gAvg = gAvg // count
+            bAvg = bAvg // count
+
+            newImg = self.image.original
+            toModify = self.image.modified
+
+            rgbColors = toModify.convert('RGB')
+            pixels = newImg.load()
+            
+            for col in range(toModify.size[0]):
+                for row in range(toModify.size[1]):
+                    r, g, b = rgbColors.getpixel((col, row))
+                    pixels[col, row] = (r & rAvg, g & gAvg, b & bAvg)            
+
+            newImg = newImg.resize(self.outPixelsSize)
+            
+            outputImage.paste(newImg, (posX, posY))
+
+            percent = '%0.2f' % (i * 100 / total)
+            print(f'{i} / {total} - {percent}%', end='\r')
+
+            posX += self.outPixelsSize[0]
+            if xRight >= toModify.size[0] - 1:
+                posX = 0
+                posY += self.outPixelsSize[1]
+
+        outputImage.save(f'out.jpg')
+        
+        return outputImage
         
     """
     Function that initializes the canvas for the images.
